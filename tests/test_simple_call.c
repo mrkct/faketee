8fc9ddb8499fcd0adf456a5e14c95e83064df8e7
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-07-12 15:32:29 +0200

    tests: add a test for invoke_function VALUE_INPUT

--- tests/test_simple_call.c
+++ tests/test_simple_call.c
@@ -50,31 +50,45 @@ int main(int argc, char **argv)
 		struct tee_ioctl_open_session_arg arg;
 		uint8_t data[sizeof(struct tee_ioctl_open_session_arg) +
 			     sizeof(struct tee_ioctl_param) * ARGS_LEN];
-	} request;
-	struct tee_ioctl_buf_data buf_data = { .buf_len = sizeof(request),
-					       .buf_ptr = (uintptr_t)&request };
-	struct tee_ioctl_open_session_arg *arg = &request.arg;
-	arg->clnt_login = TEE_IOCTL_LOGIN_PUBLIC;
-	arg->num_params = 0;
-	memcpy(arg->uuid, TA_UUID, sizeof(TA_UUID));
-	if ((rc = ioctl(fd, TEE_IOC_OPEN_SESSION, &buf_data))) {
+	} open_session_arg;
+	struct tee_ioctl_buf_data open_session_buf_data = { .buf_len = sizeof(open_session_arg), .buf_ptr = (uintptr_t)&open_session_arg };
+	struct tee_ioctl_open_session_arg *open_session = &open_session_arg.arg;
+	open_session->clnt_login = TEE_IOCTL_LOGIN_PUBLIC;
+	open_session->num_params = 0;
+	memcpy(open_session->uuid, TA_UUID, sizeof(TA_UUID));
+	if ((rc = ioctl(fd, TEE_IOC_OPEN_SESSION, &open_session_buf_data))) {
 		perror("failed ioctl for OPEN_SESSION");
 		return rc;
 	}
 	printf("session opened\n\tret: %u\n\torigin: %u\n\tsession_id: %u\n",
-	       arg->ret, arg->ret_origin, arg->session);
-	unsigned int session_id = arg->session;
+	       open_session->ret, open_session->ret_origin, open_session->session);
+	unsigned int session_id = open_session->session;
 
-	/*
-    const int VALUE_BEFORE_INCREMENT = 5;
-    struct tee_invoke_func_arg invoke_func_arg;
-    if (rc = ioctl(fd, TEE_IOC_INVOKE, &invoke_func_arg)) {
+	const size_t arg_size = sizeof(struct tee_ioctl_invoke_arg) + 1 * sizeof(struct tee_ioctl_param);
+	union {
+		struct tee_ioctl_invoke_arg arg;
+		uint8_t data[arg_size];
+	} invoke_arg;
+	struct tee_ioctl_buf_data buf_data = {.buf_len = sizeof(invoke_arg), .buf_ptr = (uintptr_t)&invoke_arg };
+	struct tee_ioctl_invoke_arg *arg = &invoke_arg.arg;
+
+	arg->session = open_session->session;
+	// ID of the function we want to request inside this TA. We hardcoded this ID in the module
+	arg->func = 1234;
+	
+	arg->num_params = 1;
+	struct tee_ioctl_param *params = (struct tee_ioctl_param *)(arg + 1);
+	params[0] = (struct tee_ioctl_param){
+		.attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INOUT, 
+		.a = 1111, .b = 2222, .c = 3333
+	};
+
+    if ((rc = ioctl(fd, TEE_IOC_INVOKE, &buf_data))) {
         perror("failed ioctl for INVOKE");
         return rc;
     }
-    int val = VALUE_BEFORE_INCREMENT + 1;
-    assert(VALUE_BEFORE_INCREMENT == val + 1);
-*/
+	printf("invoke_request: res=%d   ret_origin=%d\n", arg->ret, arg->ret_origin);
+	printf("a=%lld b=%lld c=%lld\n", params[0].a, params[0].b, params[0].c);
 
 	struct tee_ioctl_close_session_arg close_session_arg;
 	close_session_arg.session = session_id;
